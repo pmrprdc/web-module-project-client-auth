a9f940c11b3eb47a294a14bd5d21321251580654
Pamir Perdeci <pamirperdeci@gmail.com>
2023-08-14 17:30:24 -0400

    crated a link inside the router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <h2>Client Auth Project</h2>
+      <Link to="./login">Login</Link>
     </div>
   );
 }
