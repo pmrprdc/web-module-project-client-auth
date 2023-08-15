c7757740f2e02348e214f7f93fb49d2ed1691eeb
Pamir Perdeci <pamirperdeci@gmail.com>
2023-08-15 15:59:17 -0400

    added basic html items such as email, password inputs, header and a submit button

--- src/App.js
+++ src/App.js
@@ -5,8 +5,37 @@ import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <h2>Client Auth Project</h2>
-      <Link to="./login">Login</Link>
+      <header>
+        <h1>FRIENDS DATABASE</h1>
+      <Link to="./login">LOGIN</Link>
+      <Link to="./login">FRIENDSLIST</Link>
+      <Link to="./login">ADDFRIEND</Link>
+      <Link to="./login">LOGOUT</Link>
+    
+      </header>
+      <h1>LOGIN   
+      </h1>
+      <label htmlFor="email">Email 
+      <input
+        type="email"
+        id="email"
+        placeholder="Enter your email"
+      />
+      </label>
+
+    <br></br>
+      <label htmlFor="email">Password 
+      
+      <input
+        type="password"
+        id="password"
+        placeholder="Enter your password"
+      />
+      </label>
+      <br></br>
+      <button>SUBMIT</button>
+      
+
     </div>
   );
 }
